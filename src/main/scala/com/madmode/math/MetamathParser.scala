5d1473976b534f80a98469f81b3ced34cf0bbec1
Dan Connolly <dconnolly@kumc.edu>
2012-06-01 01:47:23 -0500

    metamath parser covering all 7 kinds of statements

--- src/main/scala/com/madmode/math/MetamathParser.scala
+++ src/main/scala/com/madmode/math/MetamathParser.scala
@@ -11,21 +11,101 @@ package com.madmode.math
 
 import scala.util.parsing.combinator.{ Parsers, RegexParsers }
 
-class MetamathLexer extends RegexParsers {
-  override val whiteSpace = "[ \t\r\n\f]+".r
-  //default: override def skipWhitespace = true
-  def comment = "\\$\\((?:[^\\$]|\\$[^)])*\\$\\)".r
+class Preliminaries extends RegexParsers {
+  /* 4.1.1 Preliminaries */
+  val ws_char_e = "[ \t\r\n\f]"
+  val comment_e = "\\$\\((?:[^\\$]|\\$[^)])*\\$\\)"
+
   def label = "[A-Za-z0-9\\-_\\.]+".r
   /** math symbol */
   def sym = "[0-9A-Za-z`~!@#%^&*()\\-_=+\\[\\]{};:'\",\\.<>/?\\\\|]+".r
-  def keyword = "\\$[{}cvfedap\\.\\-()\\[\\]]".r
 }
 
-class MetamathParser extends MetamathLexer {
-  def token = comment | label | sym | keyword
-  def tokens = token .*
-  def file = phrase(tokens)
+class Preprocessing extends Preliminaries {
+  /**
+   * "Comments are ignored (treated like white space) for the
+   * purpose of parsing."
+   */
+  override val whiteSpace = ("(?:" + ws_char_e + "|" + comment_e + ")+").r
+
+  /**
+   * TODO: implement file inclusion.
+   */
+  def file_inclusion_command = "$[" ~ filename ~ "$]"
+  def filename = sym
 }
+
+class BasicSyntax extends Preprocessing {
+  def database = block
+  def block: Parser[List[Statement]] = (
+    statement ~ block ^^ { case s ~ b =>
+      s :: b }
+    | ("${" ~> block <~ "$}") ~ block ^^ { case b1 ~ b2 =>
+      b1 ++ b2 }
+    | success(List())
+  )
+
+  def statement: Parser[Statement] = (
+    declare_variables | declare_constants | hypothesis
+    | disjoint_variable_restriction | assertion )
+
+  /* TODO: The same math symbol may not occur twice
+   * in a given $v or $c statement. */
+  /* TODO: A constant must be declared in the
+   * outermost block and may not be declared a second time. */
+
+  def declare_variables = "$v" ~> sym .* <~ "$." ^^ { case syms =>
+    VariableDecl(syms) }
+  def declare_constants = "$c" ~> sym .* <~ "$." ^^ { case syms =>
+    ConstantDecl(syms) }
+
+  def hypothesis = (
+    label ~ (("$f" ~> active_constant ~ active_variable) ^^
+	     { case k ~ v => VariableType(k, v) }
+	     | ("$e" ~> active_constant ~ rep(active_symbol)) ^^
+	     { case k ~ expr => Logical(k, expr) }) <~ "$.") ^^ {
+    case l ~ expr => Indexed(l, expr)
+  }
+
+  def active_symbol = active_constant | active_variable
+  def active_constant = sym /* TODO: active */
+  def active_variable = sym /* TODO: active */
+
+  def disjoint_variable_restriction = "$d" ~> rep(active_variable) <~ "$." ^^ {
+    DisjoinVariables(_) }
+
+  def assertion = (
+    label ~ (("$a" ~> active_constant ~ rep(active_symbol)) ^^
+	     { case k ~ expr => Axiom(k, expr) }
+	     | ("$p" ~> active_constant ~ rep(active_symbol)
+		~ "$=" ~ rep(label)) ^^
+	     { case k ~ expr ~ "$=" ~ pf =>
+	       Theorem(k, expr, pf) }) <~ "$.") ^^ {
+    case l ~ expr => Indexed(l, expr)
+  }
+
+  /* TODO: A $f, $e, or $d statement is active from ... */
+}
+
+
+sealed abstract class Statement
+
+sealed abstract class Declaration extends Statement
+case class VariableDecl(symbols: List[String]) extends Declaration
+case class ConstantDecl(symbols: List[String]) extends Declaration
+case class DisjoinVariables(symbols: List[String]) extends Declaration
+
+case class Indexed(label: String, expr: Expression) extends Statement
+
+sealed abstract class Expression
+sealed abstract class Hypothesis extends Expression
+case class VariableType(c: String, t: String) extends Hypothesis
+case class Logical(mark: String, symbols: List[String]) extends Hypothesis
+case class Axiom(mark: String, symbols: List[String]) extends Expression
+/* TODO: inference, i.e. with hypotheses. */
+case class Theorem(mark: String, symbols: List[String], proof: List[String]
+		 ) extends Expression
+
 
 object ExampleApp extends App {
   println("Hello, Metamath Tools.")
--- src/test/scala/MPTest.scala
+++ src/test/scala/MPTest.scala
@@ -3,7 +3,7 @@ package com.madmode.math
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 
-class TestLexer extends MetamathLexer {
+class TestLexer extends Preliminaries {
   def test_sym(doc: String): String = {
     parseAll(sym, doc) match {
       case Success(s, _) => s
@@ -13,11 +13,11 @@ class TestLexer extends MetamathLexer {
 }
 
 
-class TestParser extends MetamathParser {
-  def test_file(doc: String): List[String] = {
-    parseAll(file, doc) match {
-      case Success(result, _) => result
-      case failure : NoSuccess => List("FAILURE:" + failure.msg)
+class TestParser extends BasicSyntax {
+  def test_file(doc: String): Either[String, List[Statement]] = {
+    parseAll(database, doc) match {
+      case Success(result, _) => Right(result)
+      case failure : NoSuccess => Left(failure.msg)
     }
   }
 }
@@ -33,13 +33,21 @@ class ParserTest extends Spec with ShouldMatchers {
       (new TestLexer().test_sym("ab c") contains "FAILURE") should equal (true);
     }
 
-    it("should tokenize a label") {
-      (new TestParser().test_file("axiom.1")) should equal (List("axiom.1"))
+    it("should parse a simple example") {
+      (new TestParser().test_file("axiom.1 $a |- x = x $.")) should equal (
+	Right(List(Indexed("axiom.1", Axiom("|-", List("x", "=", "x"))))) )
     }
 
-    it("should tokenize a simple example") {
-      (new TestParser().test_file("axiom.1 $a |- x = x $.")) should equal (
-List("axiom.1", "$a", "|-", "x", "=", "x", "$.") )
+    it("should parse a larger example") {
+      (new TestParser().test_file("""
+stmt1 $f wff P $.
+stmt2 $f set x $.
+stmt3 $e |- ( P -> Q ) $.
+""")) should equal (
+	Right(List(Indexed("stmt1",VariableType("wff","P")),
+		   Indexed("stmt2",VariableType("set","x")),
+		   Indexed("stmt3",Logical("|-",List("(", "P", "->", "Q", ")")))
+		   )) )
     }
 
   }
