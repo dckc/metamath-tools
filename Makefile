beb335407d39efaaeba3a41888697ac9f938ab4e
Dan Connolly <http://www.madmode.com/>
2012-10-26 01:54:02 -0500

    parsing up to test_basic_syntax::keyword_statement_with_doc works:
    -*- mode: compilation; default-directory: "~/projects/metamath-tools/" -*-
    Compilation started at Fri Oct 26 01:51:34
    
    make -k check
    ./mmparse
    
    running 12 tests
    test test_basic_syntax::keyword_statement_with_doc ... ok
    test test_basic_syntax::simple_keyword ... ok
    test test_preliminaries::simple_comment ... ok
    test test_basic_syntax::simple_keyword_statement ... ok
    test test_basic_syntax::simple_expression ... ok
    test test_preliminaries::bad_char ... ok
    test test_preliminaries::bad_symbol ... ok
    test test_basic_syntax::expression_before_dot ... ok
    test test_preliminaries::bad_label ... ok
    test test_preliminaries::simple_symbol_with_space ... ok
    test test_preliminaries::simple_label ... ok
    test test_preliminaries::simple_comments ... ok
    
    result: ok. 12 passed; 0 failed; 0 ignored
    
    
    Compilation finished at Fri Oct 26 01:51:34

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ RUST=rustc
 check: mmparse
 	./mmparse
 
+#RUST_THREADS=1 RUST_LOG=mmparse::test_basic_syntax ./mmparse
+#RUST_THREADS=1 RUST_LOG=mmparse::preprocessing,mmparse::test_preliminaries ./mmparse
+
 mmparse: mmparse.rs
 	$(RUST) mmparse.rs --test
--- mmparse.rs
+++ mmparse.rs
@@ -15,11 +15,17 @@ info: central/rparse (0188129D-F459-4EA4-A928-A5BA5632EF2E) [parse, parsing]
 https://github.com/jesse99/rparse
 */
 extern mod rparse;
-use rparse::{Parser, ParseFailed, Combinators, StringParsers, EOT,
-             ret, match1, match0, scan };
+use rparse::{Parser, StringParsers, GenericParsers, Combinators,
+             ParseFailed,
+             ret, match1, match0, scan, or_v, seq4 };
 
-mod preprocessing {
-    pub fn space() -> Parser<@~str> { match0(|ch| ws_char.contains_char(ch)) }
+mod preliminaries {
+    pub fn white_space() -> Parser<@~str> {
+        match0(|ch| ws_char.contains_char(ch))
+    }
+    const ws_char: &str = &" \t\r\n\x0A";
+
+    // TODO: pub enum Label = @~str; // constrained syntax
 
     pub fn label() -> Parser<@~str> {
         match1(|ch| {  // why isn't match1 pure?
@@ -39,7 +45,6 @@ mod preprocessing {
     pub fn comment() -> Parser<@~str> {
         "$( ".lit().then(scan(to_close)).s0()
     }
-    //TODO var last_comment: Option[String] = None
       
     pure fn to_close(chars: @[char], index: uint) -> uint {
         enum S { Start, Space, Dollar };
@@ -97,14 +102,33 @@ mod preprocessing {
         &[('\u0021', '\u0023'), ('\u0025', '\u007f')];
     const ascii_printable_but_cparen: Ranges =
         &[('\u0021', '\u0028'), ('\u002A', '\u007f')];
-    const ws_char: &str = &" \t\r\n\x0A";
     const label_extra: &str = &"'-_.";
 }
                
 #[cfg(test)]
 // 4.1.1 Preliminaries
 mod test_preliminaries {
-    use mod preprocessing::*;
+    use mod preliminaries::*;
+
+    /** The only characters that are allowed to appear in a Metamath
+    source file are the 94 printable characters on standard ascii
+    keyboards ...  plus the following non-printable (white space)
+    characters: space, tab, car- riage return, line feed, and form
+    feed.*/
+    #[test]
+    fn bad_char() {
+        let actual = comment()
+            .everything(white_space())
+            .parse(@~"f", @"$( x \u0000 is not allowed $)");
+        match actual {
+	  Ok(_) => fail,
+	  Err(pf) => {
+            debug!("parse failed at column: %u", pf.col);
+            assert pf.col == 4
+          }
+        }
+    }
+
 
     #[test]
     fn simple_label() {
@@ -121,7 +145,7 @@ mod test_preliminaries {
     #[test]
     fn simple_symbol_with_space() {
         let actual = math_symbol()
-            .everything(space())
+            .everything(white_space())
             .parse(@~"some_file", @" <*> ");
         assert *actual.get() == ~"<*>";
     }
@@ -129,7 +153,7 @@ mod test_preliminaries {
     #[test]
     fn bad_symbol() {
         let actual = math_symbol()
-            .everything(space())
+            .everything(white_space())
             .parse(@~"some_file", @"<$>");
         assert actual.is_err();
     }
@@ -137,62 +161,186 @@ mod test_preliminaries {
     #[test]
     fn simple_comment() {
         let actual = comment()
-            .everything(space())
+            .everything(white_space())
             .parse(@~"f", @"$( c1 $)");
         match actual {
           Ok(s) => {
             debug!("comment: [%s]", *s);
             assert s == @~"c1 $)"
           },
-          Err(_) => fail ~"parsing comment"
+          Err(pf) => { fail fmt!("%?", pf) }
         }
     }
 
     #[test]
     fn simple_comments() {
         let actual = comments()
-            .everything(space())
+            .everything(white_space())
             .parse(@~"f", @" $( c1 $)$( c2 $)");
         match actual {
           Ok(s) => {
             debug!("comment: [%s]", *s);
             assert s == @~"c2 $)"
           },
-          Err(_) => fail ~"parsing comments"
+          Err(pf) => { fail fmt!("%?", pf) }
         }
     }
 
-    /** The only characters that are allowed to appear in a Metamath
-    source file are the 94 printable characters on standard ascii
-    keyboards ...  plus the following non-printable (white space)
-    characters: space, tab, car- riage return, line feed, and form
-    feed.*/
-    #[test]
-    fn bad_char() {
-        let actual = comment()
-            .everything(space())
-            .parse(@~"f", @"$( x \u0000 is not allowed $)");
-        match actual {
-	  Ok(_) => fail,
-	  Err(pf) => {
-            debug!("parse failed at column: %u", pf.col);
-            assert pf.col == 4
-          }
+}
+
+
+mod basic_syntax {
+    use mod preliminaries::*;
+
+    pub enum ScopingStatement {
+        BlockStart,
+        BlockEnd
+    }
+
+    pub fn scoping_statement() -> Parser<ScopingStatement> {
+        comments().optional()
+            .then(("${".litv(BlockStart))
+                  .or("$}".litv(BlockEnd)))
+    }
+
+    struct KeywordStatement {
+        doc: Option<@~str>,
+        label: Option<@~str>,
+        kw: Keyword,
+        expr: @~[@~str],
+        pf: Option<@~Proof>
+    }
+
+/*
+    impl KeywordStatement: ToStr {
+        pure fn to_str() {
+            fmt!("%s",
+                 (match doc { Some(@s) => "$( " + s + " $)\n", None => "" }),
+                 (match label { Some(@s) => s, None => "" }),
+*/
+
+    pub fn keyword_statement() -> Parser<@~KeywordStatement> {
+        do comments().optional().thene() |doc_o| {
+            debug!("kws after comments");
+            do seq4(label().optional(),
+                    keyword(),
+                    expression(),
+                    proof().optional()) |l_o, kw, e, pf_o| {
+                debug!("kws after seq4");
+                Ok(@~KeywordStatement{
+                    doc: doc_o, label: l_o, kw: kw, expr: e, pf: pf_o})
+            }
+            .thene(|ks| "$.".litv(ks))
         }
     }
-}
-/*
-    it("""A Metamath database consists of a sequence of three kinds of tokens
-       separated by white space.""") {
-      val r = ReportingParseRunner(p.statements).run("axiom.1 $a |- x = x $.")
-      val parts = r.result match {
-	case Some(result) => result
-	case None => r.parseErrors
-      }
-      parts should equal (
-	p.StatementParts(None,Some("axiom.1"),"$a",
-			 List("|-", "x", "=", "x"),None)
-      )
+
+    enum Keyword {
+        c, v, f, e, d, a, p
     }
-  }
-*/
+
+    pub fn keyword() -> Parser<Keyword> {
+        let lookup = &[("$c", c), ("$v", v),
+                       ("$f", f), ("$e", e),
+                       ("$d", d),
+                       ("$a", a), ("$p", p)];
+        let parsers: @~[Parser<Keyword>] =
+            @lookup.map(|p| match p { &(tok, kw) => tok.litv(kw) });
+        or_v(parsers).s0()
+    }
+
+    pub fn expression() -> Parser<@~[@~str]> {
+        math_symbol().list(white_space().or(comment())).s0()
+    }
+
+    pub fn proof() -> Parser<@~Proof> {
+        //@@@
+        "$=".litv(@~Proof{labels: @~[], digits: None})
+    }
+
+    pub struct Proof {
+        labels: @~[@~str],
+        digits: Option<@~str>
+    }
+
+}
+
+
+#[cfg(test)]
+mod test_basic_syntax {
+    use mod basic_syntax::*;
+
+    #[test]
+    fn simple_keyword() {
+        let actual = keyword()
+            .everything(white_space())
+            .parse(@~"ex1", @" $a ");
+        match actual {
+          Ok(kw) => {
+            debug!("keyword: [%?]", kw);
+            match kw { a => (), _ => fail };
+          },
+          Err(pf) => { fail fmt!("%?", pf) }
+        }
+    }
+
+    #[test]
+    fn simple_expression() {
+        let actual = expression()
+            .everything(white_space())
+            .parse(@~"ex1", @" |- x = x");
+        match actual {
+          Ok(expr) => {
+            debug!("expression: [%?]", expr);
+            assert expr.len() == 4;
+            assert *expr[0] == ~"|-";
+            ()
+          },
+          Err(pf) => { fail fmt!("%?", pf) }
+        }
+    }
+
+    #[test]
+    fn expression_before_dot() {
+        let actual = expression()
+            .parse(@~"ex2", @"|- x = x $.");
+        match actual {
+          Ok(s) => { debug!("expression: [%?]", s); () },
+          Err(pf) => { fail fmt!("%?", pf) }
+        }
+    }
+
+    #[test]
+    fn simple_keyword_statement() {
+        let actual = keyword_statement()
+            .everything(white_space())
+            .parse(@~"ax1", @"axiom.1 $a |- x = x $.");
+        match actual {
+          Ok(st) => {
+            assert st.doc == None;
+            assert st.label == Some(@~"axiom.1");
+            match st.kw { a => (), _ => fail };
+            assert st.expr.len() == 4;
+            debug!("keyword_statement: [%?]", st);
+            ()
+          },
+          Err(pf) => { fail fmt!("%?", pf) }
+        }
+    }
+
+    #[test]
+    fn keyword_statement_with_doc() {
+        let actual = keyword_statement()
+            .everything(white_space())
+            .parse(@~"ax1",
+                   @" $( irrelevant... $) $( doc... $) axiom.1 $a |- x = x $.");
+        match actual {
+          Ok(st) => {
+            assert st.doc == Some(@~"doc... $)");
+            debug!("keyword_statement: [%?]", st);
+            ()
+          },
+          Err(pf) => { fail fmt!("%?", pf) }
+        }
+    }
+
+}
