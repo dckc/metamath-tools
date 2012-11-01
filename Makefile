d01f94523970c62419130fd980b22c0fad319f16
Dan Connolly <http://www.madmode.com/>
2012-10-31 21:18:24 -0500

    toward FSM implementation of metamath language: one test passes

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 # rust-lang.org
 RUST=rustc
+#RUST=RUST_LOG=rustc=0,::rt::backtrace rustc
 
 RUST_THREADS=1
 RUST_LOG=
+RUST_LOG=mmfsm
 #RUST_LOG=mmparse::basic_syntax,mmparse::test_basic_syntax
 #RUST_LOG=mmparse::basic_syntax,mmparse::preprocessing,rparse::parsers,mmparse::test_preliminaries
 #...
+
+check2: mmfsm
+	RUST_THREADS=$(RUST_THREADS) RUST_LOG=$(RUST_LOG) ./mmfsm
+
+mmfsm: mmfsm.rs
+	$(RUST) mmfsm.rs --test
+
 
 check: mmparse
 	RUST_THREADS=$(RUST_THREADS) RUST_LOG=$(RUST_LOG) ./mmparse
--- mmfsm.rs
+++ mmfsm.rs
@@ -1,10 +1,90 @@
+/**
+start = space start
+      | statement start
+      | comment start
+      | EOF
+
+statement = '${' space+ | '$}' tspace | keyword_statement
+keyword_statement = label? keyword expr proof? '$.' tspace
+label = label_char+ space+
+keyword = ($a | $p | ...) space+
+expr = symbol*
+symbol = symbol_char+ space+
+
+comment = '$(' space (not_$) space)* '$)' tspace
+ispace = (space+ comment?)*  # inter-token space
+
+tspace = space+ | EOF
+
+proof = '$=' space+ label+ '$'  # TODO: compressed proof
+ */
+use core::vec::{any};
 
 extern mod std;
 use std::rope;
 
 fn each_statement(text: rope::Rope) -> bool {
-    for rope::loop_chars(text) |ch| {
+    enum CharClass { Space, Printable, BadChar }
+
+    enum State {
+        Start,
+        StLabel,
+        StKeyword,
+        StExpr,
+        TSpace,
+
+        BadSyntax, // line, col?
+    }
+
+    let mut state = Start;
+    let mut ch1 = '\u0000'; // LL(2)
+
+    do rope::loop_chars(text) |ch| {
+        let cclass =
+            if(" \t\r\n\x0A".contains_char(ch)) { Space }
+            else if (ch > '\u007F' || ch < ' ') { BadChar }
+            else { Printable };
+
+        let is_label = any(&[('A', 'Z'), ('a', 'z'), ('0', '9')],
+                           |r| match r { &(lo, hi) => ch >= lo && ch <= hi })
+            || "'-_.".contains_char(ch);
+
+        state = match (state, cclass, is_label, ch1, ch) {
+          (_, BadChar, _, _, _) => BadSyntax,
+
+          (Start, Space, _, _, _) => Start,
+          (Start, Printable, true, _, _) => StLabel,
+          (Start, _, _, _, _) => BadSyntax,  // not implemented
+          
+          (StLabel, Printable, true, _, _) => StLabel,
+          (StLabel, Space, _, _, _) => StKeyword,
+          (StLabel, _, _, _, _) => BadSyntax,
+          
+          (StKeyword, Space, _, _, _) => StKeyword,
+          (StKeyword, Printable, _, '$', 'a') => StExpr,
+          (StKeyword, _, _, _, _) => BadSyntax,
+
+          (StExpr, _, _, '$', '.') => TSpace,
+          (StExpr, _, _, '$', _)  => BadSyntax,
+          (StExpr, _, _, _, _) => StExpr,
+
+          (TSpace, Space, _, _, _) => TSpace,
+          (TSpace, Printable, _, _, _) => BadSyntax,  // not implemented
+
+          (BadSyntax, _, _, _, _) => fail fmt!("unexpected state: %?", state)
+        };
+
+        ch1 = ch;
         debug!("hi: %c", ch);
+        match state {
+            BadSyntax => false,
+            _ => true
+        }
+    };
+
+    match state {
+      TSpace => true,
+      _ => false
     }
 }
 
